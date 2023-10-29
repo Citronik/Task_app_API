05e509ed7056d64d1be2d03ca6482ad80a8e10c6
Citronik <perdoch.filip@gmail.com>
2023-10-29 18:49:30 +0100

    added oat auth

--- config/auth.ts
+++ config/auth.ts
@@ -1,27 +1,29 @@
 import type { AuthConfig } from '@ioc:Adonis/Addons/Auth'
 
-/*
-|--------------------------------------------------------------------------
-| Authentication Mapping
-|--------------------------------------------------------------------------
-|
-| List of available authentication mapping. You must first define them
-| inside the `contracts/auth.ts` file before mentioning them here.
-|
-*/
 const authConfig: AuthConfig = {
-  guard: 'web',
+  guard: 'api',
   guards: {
+    api: {
+      driver: 'oat',
+      provider: {
+        driver: 'lucid',
+        identifierKey: 'id',
+        uids: ['email'],
+        model: () => import('App/Models/User'),
+      },
+      tokenProvider: {
+        type: 'api',
+        driver: 'database',
+        table: 'api_tokens',
+        foreignKey: 'user_id',
+      },
+    },
     web: {
       driver: 'session',
       provider: {
-
         driver: 'lucid',
-
         identifierKey: 'id',
-
         uids: ['username', 'email'],
-
         model: () => import('App/Models/User'),
       },
     },
--- contracts/auth.ts
+++ contracts/auth.ts
@@ -17,34 +17,16 @@ declare module '@ioc:Adonis/Addons/Auth' {
     }
   }
 
-  /*
-  |--------------------------------------------------------------------------
-  | Guards
-  |--------------------------------------------------------------------------
-  |
-  | The guards are used for authenticating users using different drivers.
-  | The auth module comes with 3 different guards.
-  |
-  | - SessionGuardContract
-  | - BasicAuthGuardContract
-  | - OATGuardContract ( Opaque access token )
-  |
-  | Every guard needs a provider for looking up users from the database.
-  |
-  */
   interface GuardsList {
-    /*
-    |--------------------------------------------------------------------------
-    | OAT Guard
-    |--------------------------------------------------------------------------
-    |
-    | OAT, stands for (Opaque access tokens) guard uses database backed tokens
-    | to authenticate requests.
-    |
-    */
+
     web: {
       implementation: SessionGuardContract<'user', 'web'>
       config: SessionGuardConfig<'user'>
+    },
+    api: {
+      implementation: OATGuardContract<'user', 'api'>
+      config: OATGuardConfig<'user'>
+      client: OATClientContract<'user'>
     }
   }
 }
