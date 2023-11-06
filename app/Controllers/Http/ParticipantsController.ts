d56ded9516f96851349ab6ccef55e480a957cb52
Citronik <perdoch.filip@gmail.com>
2023-11-06 18:59:57 +0100

    prettier formated

--- app/Controllers/Http/ParticipantsController.ts
+++ app/Controllers/Http/ParticipantsController.ts
@@ -5,16 +5,16 @@ import Room from 'App/Models/Room'
 import CreateParticipants from 'App/Validators/Room/CreateParticipantValidator'
 
 export default class ParticipantsController {
-  public async getRoomParticipants ({ params, bouncer } : HttpContextContract) {
-    console.log('Get all participants of a '+ params.id +' Room')
+  public async getRoomParticipants({ params, bouncer }: HttpContextContract) {
+    console.log('Get all participants of a ' + params.id + ' Room')
     const room = await Room.findOrFail(params.id)
     await bouncer.with('RoomPolicy').authorize('view', room)
     const participants = await room.related('participants').query()
     return participants
   }
 
-  public async addRoomParticipant ({ params, bouncer, request } : HttpContextContract) {
-    console.log('Add a participant to a '+ params.id +' Room')
+  public async addRoomParticipant({ params, bouncer, request }: HttpContextContract) {
+    console.log('Add a participant to a ' + params.id + ' Room')
     const room = await Room.findOrFail(params.id)
     await bouncer.with('RoomPolicy').authorize('update', room)
     const payload = await request.validate(CreateParticipants)
